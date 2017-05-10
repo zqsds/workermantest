afd823c58aadc96366c068f375f1acd976bcfaa3
xiaofeng <xiao_feng@hunliji.com>
2017-05-10 14:47:59 +0800

    php

--- workermantest/http_test.php
+++ workermantest/http_test.php
@@ -19,7 +19,7 @@ $http_worker->onMessage = function($connection, $data)
 {
     // 向浏览器发送hello world
     var_dump($_GET,$_POST);
-    $connection->send('hello world');
+    $connection->send("$_POST");
 };
 
 // 运行worker
