ef07b8fdf0d4c9b93b77f7e1b1fb906f38d37aab
xiaofeng <xiao_feng@hunliji.com>
2017-05-11 13:50:22 +0800

    php

--- workermantest/onclose.php
+++ workermantest/onclose.php
@@ -8,7 +8,7 @@
 use Workerman\Worker;
 require_once __DIR__ . '/Workerman/Autoloader.php';
 
-$worker = new Worker('websocket://0.0.0.0:8484');
+$worker = new Worker('websocket://0.0.0.0:8485');
 $worker->onMessage = function($connection, $data)
 {
     $connection->close("hello\n");
