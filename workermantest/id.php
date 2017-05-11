5edd2282cb72889859f312c47aed0d472cbb4c0a
xiaofeng <xiao_feng@hunliji.com>
2017-05-11 10:02:55 +0800

    php

---
+++ workermantest/id.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/11
+ * Time: 9:50
+ */
+use Workerman\Worker;
+require_once __DIR__."/Workerman/Autoloader.php";
+
+$worker = new Worker('tcp://0.0.0.0:8484');
+
+$worker -> onMessage = function ($connection){
+    echo $connection -> id;
+};
+
+Worker::runAll();
