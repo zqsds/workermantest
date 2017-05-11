2488f5b0ae74a74501e28b5754559ccd166a3a85
xiaofeng <xiao_feng@hunliji.com>
2017-05-11 14:00:49 +0800

    php

--- workermantest/destroy.php
+++ workermantest/destroy.php
@@ -8,7 +8,7 @@
 use Workerman\Worker;
 require_once __DIR__ . '/Workerman/Autoloader.php';
 
-$worker = new Worker('websocket://0.0.0.0:8484');
+$worker = new Worker('websocket://0.0.0.0:8486');
 $worker->onMessage = function($connection, $data)
 {
     // if something wrong
