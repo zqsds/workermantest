cd928b3e0e46155e1f2649d2a775e71cab7d78af
xiaofeng <xiao_feng@hunliji.com>
2017-10-26 10:02:33 +0800

    改动

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by xiao_feng@hunliji.com
+ * Date: 2017/10/26
+ * Time: 9:58
+ *   ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ *    ┃　　　┃
+ *    ┃　　　┃
+ *    ┃　　　┗━━━┓
+ *    ┃　　　　　　　┣┓
+ *    ┃　　　　　　　┏┛
+ *    ┗┓┓┏━┳┓┏┛
+ *      ┃┫┫　┃┫┫
+ *      ┗┻┛　┗┻┛
+ *        神兽保佑
+ *        代码无BUG!
+ */
+echo '认证成功！！';
+echo exec("../github_pull2.sh");
+//var_dump(exec("./github_pull.sh"));
+echo date("Y-m-d H:i:s");
+
+
