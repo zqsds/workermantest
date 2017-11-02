f1787b9728f454612c72af61062d039499f3e967
xiaofeng <xiao_feng@hunliji.com>
2017-11-02 11:02:16 +0800

    测试

---
+++ start.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by xiao_feng@hunliji.com
+ * Date: 2017/11/1
+ * Time: 10:49
+ *   ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ *    ┃　　　┃
+ *    ┃　　　┃
+ *    ┃　　　┗━━━┓
+ *    ┃　　　　　　　┣┓
+ *    ┃　　　　　　　┏┛
+ *    ┗┓┓┏━┳┓┏┛
+ *      ┃┫┫　┃┫┫
+ *      ┗┻┛　┗┻┛
+ *        神兽保佑
+ *        代码无BUG!
+ */
+use Workerman\Worker;
+require_once 'Workerman/Autoloader.php';
+
+$global_uid = 0;
+
+// 当客户端连上来时分配uid，并保存连接，并通知所有客户端
+function handle_connection($connection) {
+    global $text_worker, $global_uid;
+    // 为这个链接分配一个uid
+    $connection->uid = ++$global_uid;
+    $connection->send("hello world!!");
+    /*foreach ($text_worker->connections as $conn) {
+        $conn->send("user[{$connection->uid}] online");
+    }*/
+}
+
+// 当客户端发送消息过来时，转发给所有人
+function handle_message($connection, $data) {
+    $connection->send("$data 有些话我只对你一个人说哦!!");
+    /*global $text_worker;
+    foreach ($text_worker->connections as $conn) {
+        $conn->send("user[{$connection->uid}] said: $data");
+    }*/
+}
+
+// 当客户端断开时，广播给所有客户端
+function handle_close($connection) {
+    global $text_worker;
+    foreach ($text_worker->connections as $conn) {
+        $conn->send("user[{$connection->uid}] logout");
+    }
+}
+
+$text_worker = new Worker("websocket://0.0.0.0:2347");
+
+$text_worker->count = 1;
+
+$text_worker->onConnect = 'handle_connection';
+$text_worker->onMessage = 'handle_message';
+$text_worker->onClose = 'handle_close';
+
+Worker::runAll();
--- test.php
+++ test.php
@@ -23,4 +23,15 @@
  *        神兽保佑
  *        代码无BUG!
  */
-echo 1111;
+use Workerman\Worker;
+require_once __DIR__.'/Workerman/Autoloader.php';
+
+$http_worker = new Worker("http://0.0.0.0:2345");
+
+$http_worker -> count = 4;
+
+$http_worker->onMessage = function ($connection,$data){
+  $connection->send("hello world");
+};
+
+Worker::runAll();
